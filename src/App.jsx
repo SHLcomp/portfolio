cee6f25d33f7fc546be9e2de950b1021abd89ee9
SHLcomp <salibahe123@gmail.com>
2025-04-29 01:35:44 +0300

    changed home path

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Error from './components/pages/error/Error';
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: '/portolio/',
     element: <Home />,
     errorElement: <Error />
   },
