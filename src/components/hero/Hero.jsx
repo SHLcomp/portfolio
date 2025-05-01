11dc5069b0996f0d9e8fb1b7b4f261a956d52efb
SHLcomp <salibahe123@gmail.com>
2025-05-02 00:17:34 +0300

    fixed a typo

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,7 @@ import pdf from './cv.pdf'
 
 const Hero = () => {
   const handleWhatsApp = () => {
-    window.open('https://wa.me/9620771407526', '_blank'); 
+    window.open('https://wa.me/9620771407526', '_blank' ); 
   };
   return (
     <div className='hero'>
@@ -13,7 +13,7 @@ const Hero = () => {
         <div className="header">
           <h4 className="greet">Hey There!</h4>
           <h1 className="act-txt">I&apos;m Sarah, a <span>Web Developer</span>.</h1>
-          <p>I am Sarah Herzallah, I&apos;m from Jordan. I am currently studiying <a href="https://www.meu.edu.jo/faculties/faculty-of-engineering/intelligent-systems-engineering/">(ISE)</a> or Intelligent Systems Engineering at <a href="https://www.meu.edu.jo/">Middle East University (MEU) </a>.</p>
+          <p>I am Sarah Herzallah, I&apos;m from Jordan. I am currently studying <a href="https://www.meu.edu.jo/faculties/faculty-of-engineering/intelligent-systems-engineering/" target='_blank' rel="noopener noreferrer">(ISE)</a> or Intelligent Systems Engineering at <a href="https://www.meu.edu.jo/" target='_blank' rel="noopener noreferrer">Middle East University (MEU) </a>.</p>
           <p>Feel free to check my portfolio, and massege me for more information :)</p>
         </div>
 
